eb5453eded3757110854c37fb138d5cae9aa8fc6
snbit <snteister@gmail.com>
2020-08-11 06:54:11 +0200

    removed middleware from AccountController constructor (and see if it works without auth)

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -28,7 +28,7 @@ class AccountController extends Controller
      */
     public function __construct ()
     {
-        $this->middleware('auth');
+//        $this->middleware('auth');
     }
 
     /**
